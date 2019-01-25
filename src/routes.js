5e2a8614a1f45822ff332e9bbc5218e7f80118c6
spencercornish <SpencerCornish@users.noreply.github.com>
2019-01-25 10:35:07 -0700

    More conf

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './containers/home';
-import About from './containers/about';
 export default () => {
     return (
         <BrowserRouter>
             <Switch>
                 <Route exact path='/' component={Home} />
-                <Route path='/about' component={About} />
             </Switch>
         </BrowserRouter>
     )
